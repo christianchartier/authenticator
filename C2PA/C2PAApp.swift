ab7d223dd5738084a90972d47bec266a9bdd7a4e
Christian Chartier <christian.chartier13@gmail.com>
2024-02-07 01:24:15 -0500

    Initial Commit

---
+++ C2PA/C2PAApp.swift
@@ -0,0 +1,17 @@
+//
+//  C2PAApp.swift
+//  C2PA
+//
+//  Created by Christian Chartier on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct C2PAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
